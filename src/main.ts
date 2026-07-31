862b6c6e0f420b44b55ad164289b458e5894cbab
yanntavares <yanntavares123@gmail.com>
2026-07-31 09:44:42 -0300

    feat: add nestjs template

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Desafio NestJS')
+    .setDescription('API para o desafio do curso de NestJS')
+    .setVersion('1.0')
+    .addTag('desafio-nestjs')
+    .addTag('Seed-a-Bit')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
